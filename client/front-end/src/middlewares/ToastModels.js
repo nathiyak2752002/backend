ec5d0dcdd17496b0047ffc951f60b7f183329b29
nathiyak2752002 <nadhiyak27@gmail.com>
2024-08-29 16:16:30 +0530

    ip

--- client/front-end/src/middlewares/ToastModels.js
+++
@@ -1,9 +0,0 @@
-import toast from "react-hot-toast"
-
-export const ToastSuccess=(params)=>{
-    return toast.success(params)
-}
-
-export const ToastError=(params)=>{
-    return toast.error(params)
-}
